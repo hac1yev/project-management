0a4ab8181ec6da30b2c37618526e02070b7aed93
hac1yev <ilkinhaciyev955@gmail.com>
2023-12-04 21:15:16 +0400

    react toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Body from './components/Body';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
+
   return (
     <div className='app'>
       <Sidebar />
       <Body />
+      <div style={{ position: 'absolute' }}>
+        <ToastContainer position="bottom-right" autoClose={2000} />
+      </div>
     </div>
   );
 };
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -4,16 +4,20 @@ import { projectSliceAction } from '../store/project-slice';
 import { useParams } from 'react-router-dom';
 import { MdDelete, MdEdit } from "react-icons/md";
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const TaskItem = ({ title, id }) => {
   const [newTaskTitle,setNewTaskTitle] = useState("");
   const dispatch = useDispatch();
   const { projectId } = useParams();
 
-  const handleClick = () => {
+  const handleDelete = () => {
     dispatch(projectSliceAction.removeTask({
       id: id,
       projectId: projectId
     }));
+    toast.error("Task deleted!");
   };
 
   const handleEditSubmit = (e) => {
@@ -23,12 +27,13 @@ const TaskItem = ({ title, id }) => {
       projectId: projectId,
       newTitle: newTaskTitle
     }));
-    setNewTaskTitle('')
+    setNewTaskTitle('');
+    toast.success("Task edited!");
   };
 
 
   const handleEdited = () => {
-    dispatch(projectSliceAction.getEditId(id))
+    dispatch(projectSliceAction.getEditId(id));
   }
 
   const handleChange = (e) => {
@@ -42,27 +47,29 @@ const TaskItem = ({ title, id }) => {
           <button onClick={handleEdited} className='task-item-button' type='button' data-bs-toggle="modal" data-bs-target="#exampleModal">
             <MdEdit />
           </button>
-          <button className='task-item-button' onClick={handleClick}>
+          <button className='task-item-button' onClick={handleDelete}>
             <MdDelete />
           </button>
         </div>
-          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <form className="modal-content" onSubmit={handleEditSubmit}>
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">New task title</h5>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  <input type="text" onChange={handleChange} className='form-control' placeholder='Enter new task title!' />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="submit" className="btn" data-bs-dismiss="modal" style={{ backgroundColor: '#121716', color: '#fff' }}>Save changes</button>
-                </div>
-              </form>
-            </div>
+
+        {/* MODAL */}
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <form className="modal-content" onSubmit={handleEditSubmit}>
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">New task title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <input type="text" onChange={handleChange} className='form-control' placeholder='Enter new task title!' />
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn" data-bs-dismiss="modal" style={{ backgroundColor: '#121716', color: '#fff' }}>Save changes</button>
+              </div>
+            </form>
           </div>
+        </div>
     </li>
   )
 }
